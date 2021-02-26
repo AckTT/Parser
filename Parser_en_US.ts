45db92e41a57aa2c6b0072c5eb1a7a089503e3b9
Nikita Lyashev <17332264+AckTT@users.noreply.github.com>
2021-02-26 13:54:07 +0300

    tr

--- Parser_en_US.ts
+++ Parser_en_US.ts
@@ -1,3 +1,48 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="Parser_en_US"></TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en">
+<context>
+    <name>PARSER::MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="16"/>
+        <source>Import</source>
+        <oldsource>Inport</oldsource>
+        <translation>Импортировать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="17"/>
+        <source>Export</source>
+        <translation>Экспортировать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="18"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="19"/>
+        <source>CTRL+O</source>
+        <translation>CTRL+О</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="20"/>
+        <source>CTRL+S</source>
+        <translation>CTRL+С</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="21"/>
+        <source>CTRL+Q</source>
+        <translation>CTRL+В</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="53"/>
+        <source>Confirmation</source>
+        <translation>Подтверждение</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="53"/>
+        <source>Are you Sure you want to exit?</source>
+        <translation>Вы уверены, что хотите выйти?</translation>
+    </message>
+</context>
+</TS>
